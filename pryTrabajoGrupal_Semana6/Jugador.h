0264ef50c40b027a2f6a3c93607ccda8e87adf44
4lberto8 <alber05.50r@gmail.com>
2024-09-13 16:40:16 -0500

    Diseño del jugador que hice en 1 imnuto

--- pryTrabajoGrupal_Semana6/Jugador.h
+++ pryTrabajoGrupal_Semana6/Jugador.h
@@ -8,15 +8,19 @@ public:
     void borrar_jugador(int x, int y)
     {
         b_c(1), f_c(1);
-        p(x + 2, y); cout << "  ";
-        p(x, y + 1); cout << "      ";
-        p(x + 2, y + 2); cout << "  ";
+        p(x, y); cout << "  ";
+        p(x + 4, y); cout << "  ";
+        p(x + 2, y + 1); cout << "  ";
+        p(x, y + 2); cout << "  ";
+        p(x + 4, y + 2); cout << "  ";
     }
     void dibuja_jugador(int x, int y)
     {
         b_c(1), f_c(10);
-        p(x + 2, y); cout << char(219) << char(219);
-        p(x, y + 1); cout << char(219) << char(219) << "  " << char(219) << char(219);
-        p(x + 2, y + 2); cout << char(219) << char(219);
+        p(x, y); cout << char(219) << char(223);
+        p(x + 4, y); cout << char(223) << char(219);
+        p(x + 2, y + 1); cout << char(219) << char(219);
+        p(x, y + 2); cout << char(219) << char(220);
+        p(x + 4, y + 2); cout << char(220) << char(219);
     }
 };
