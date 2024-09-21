b80d75c6b977b95714b340803e9e99cfbbfeea26
Lieserl Ayala <lieserlnoemi89@gmail.com>
2024-09-20 21:42:56 -0500

    creacion de recurso

---
+++ pryTrabajoGrupal_Semana6/Recurso.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ArregloEnemigo.h"
+#include "ArregloArbol.h"
+#include "ArregloAgua.h"
+#include "ArregloSemillas.h"
+#include "ArregloReciclable.h"
+
+class Recurso{
+
+
+
+};
