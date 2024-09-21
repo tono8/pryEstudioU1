14ff36b8f170caaa80b325a5183dfbaa101b89a8
Lieserl Ayala <lieserlnoemi89@gmail.com>
2024-09-20 23:19:17 -0500

    Revisar

--- pryTrabajoGrupal_Semana6/Recurso.h
+++ pryTrabajoGrupal_Semana6/Recurso.h
@@ -8,7 +8,7 @@
 #include "ArregloSemillas.h"
 #include "ArregloReciclable.h"
 
-class Recurso{
+class Recurso {
 	ArrSemillas semillasArr;
 	ArrAgua aguaArr;
 	ArrEnemigo enemigoArr;
@@ -18,24 +18,24 @@ public:
 	}
 	void generarRecursos() {
 		for (int i = 0; i < 3; i++) {
-			int x = rand() % 120;
-			int y = rand() % 36;
+			int x = rand() % 87;
+			int y = rand() % 33;
 			Semillas* nuevaSemilla = new Semillas(x, y);
 			semillasArr.agregarSemillas(nuevaSemilla);
 			nuevaSemilla->dibujar_semilla();
 		}
 		for (int i = 0; i < 3; ++i) {
-			int x = rand() % 120;
-			int y = rand() % 36;
+			int x = rand() % 87;
+			int y = rand() % 33;
 			Agua* nuevoAgua = new Agua(x, y);
 			aguaArr.agregarAgua(nuevoAgua);
 			nuevoAgua->dibujar_agua(x, y);
 		}
 
 		for (int i = 0; i < 3; ++i) {
-			int x = rand() % 120;
-			int y = rand() % 36;
-			Enemigo* nuevoEnemigo = new Enemigo(x, y); 
+			int x = rand() % 87;
+			int y = rand() % 33;
+			Enemigo* nuevoEnemigo = new Enemigo(x, y);
 			enemigoArr.agregarEnemigo(nuevoEnemigo);
 			nuevoEnemigo->dibujar_enemigo();
 		}
